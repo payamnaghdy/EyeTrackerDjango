20e4c2b52b9943aad90da14f863ecd7345b3ff2c
payamnaghdy <pnaghdy@gmail.com>
2019-07-19 02:22:40 +0430

    Data shape changed to [2]

--- statics/circle.js
+++ statics/circle.js
@@ -1,188 +1,188 @@
-const dataset = {
-    train: {
-      n: 0,
-      x: null,
-      y: null,
-    },
-    val: {
-      n: 0,
-      x: null,
-      y: null,
-    },
-  }
+// const dataset = {
+//     train: {
+//       n: 0,
+//       x: null,
+//       y: null,
+//     },
+//     val: {
+//       n: 0,
+//       x: null,
+//       y: null,
+//     },
+//   }
   
-  function getImage() {
-    // Capture the current image in the eyes canvas as a tensor.
-    return tf.tidy(function() {
-      const image = tf.fromPixels($('#eyes')[0]);
-      // Add a batch dimension:
-      const batchedImage = image.expandDims(0);
-      // Normalize and return it:
-      return batchedImage.toFloat().div(tf.scalar(127)).sub(tf.scalar(1));
-    });
-  }
+//   function getImage() {
+//     // Capture the current image in the eyes canvas as a tensor.
+//     return tf.tidy(function() {
+//       const image = tf.fromPixels($('#eyes')[0]);
+//       // Add a batch dimension:
+//       const batchedImage = image.expandDims(0);
+//       // Normalize and return it:
+//       return batchedImage.toFloat().div(tf.scalar(127)).sub(tf.scalar(1));
+//     });
+//   }
   
 
-function mymove(t,l){
-    const circle = $('#circle');
-    circle.css('left',l + 'px');
-    circle.css('top',t + 'px'); 
-}
+// function mymove(t,l){
+//     const circle = $('#circle');
+//     circle.css('left',l + 'px');
+//     circle.css('top',t + 'px'); 
+// }
 
-function captureExample(t,l) {
-  // Take the latest image from the eyes canvas and add it to our dataset.
-  tf.tidy(function() {
-    const image = getImage();
-    if(image == null){
-      return
-    }
-  //   const circPos = tf.tensor1d([l, t]).expandDims(0);
-    const circPos = tf.tensor1d([(l / $(window).width()) * 2 - 1, (t / $(window).width())    * 2 - 1]).expandDims(0);
+// function captureExample(t,l) {
+//   // Take the latest image from the eyes canvas and add it to our dataset.
+//   tf.tidy(function() {
+//     const image = getImage();
+//     if(image == null){
+//       return
+//     }
+//   //   const circPos = tf.tensor1d([l, t]).expandDims(0);
+//     const circPos = tf.tensor1d([(l / $(window).width()) * 2 - 1, (t / $(window).width())    * 2 - 1]).expandDims(0);
 
-    // Choose whether to add it to training (80%) or validation (20%) set:
-    const subset = dataset[Math.random() > 0.2 ? 'train' : 'val'];
+//     // Choose whether to add it to training (80%) or validation (20%) set:
+//     const subset = dataset[Math.random() > 0.2 ? 'train' : 'val'];
 
-    if (subset.x == null) {
-      // Create new tensors
-      subset.x = tf.keep(image);
-      subset.y = tf.keep(circPos);
-    } else {
-      // Concatenate it to existing tensors
-      const oldX = subset.x;
-      const oldY = subset.y;
+//     if (subset.x == null) {
+//       // Create new tensors
+//       subset.x = tf.keep(image);
+//       subset.y = tf.keep(circPos);
+//     } else {
+//       // Concatenate it to existing tensors
+//       const oldX = subset.x;
+//       const oldY = subset.y;
       
-      subset.x = tf.keep(oldX.concat(image, 0));
-      subset.y = tf.keep(oldY.concat(circPos, 0));
-    }
+//       subset.x = tf.keep(oldX.concat(image, 0));
+//       subset.y = tf.keep(oldY.concat(circPos, 0));
+//     }
 
-    // Increase counter
-    subset.n += 1;
-    if(subset.n > 10)
-    {
-      console.log(dataset)
+//     // Increase counter
+//     subset.n += 1;
+//     if(subset.n > 10)
+//     {
+//       console.log(dataset)
       
-      fitModel()
-      setInterval(moveTarget, 100);
-    }
-  });n
-    }
-  //   const circPos = tf.tensor1d([l, t]).expandDims(0);
-    const circPos = tf.tensor1d([(l / $(window).width()) * 2 - 1, (t / $(window).width())    * 2 - 1]).expandDims(0);
+//       fitModel()
+//       setInterval(moveTarget, 100);
+//     }
+//   });n
+//     }
+//   //   const circPos = tf.tensor1d([l, t]).expandDims(0);
+//     const circPos = tf.tensor1d([(l / $(window).width()) * 2 - 1, (t / $(window).width())    * 2 - 1]).expandDims(0);
 
-    // Choose whether to add it to training (80%) or validation (20%) set:
-    const subset = dataset[Math.random() > 0.2 ? 'train' : 'val'];
+//     // Choose whether to add it to training (80%) or validation (20%) set:
+//     const subset = dataset[Math.random() > 0.2 ? 'train' : 'val'];
 
-    if (subset.x == null) {
-      // Create new tensors
-      subset.x = tf.keep(image);
-      subset.y = tf.keep(circPos);
-    } else {
-      // Concatenate it to e
-}
+//     if (subset.x == null) {
+//       // Create new tensors
+//       subset.x = tf.keep(image);
+//       subset.y = tf.keep(circPos);
+//     } else {
+//       // Concatenate it to e
+// }
 
 
 
-function circlemove(){
-    var l = 1;
-    var t = 0;
-    var sw = false;
-    var counter = 0;
-    var w = window.innerWidth - 50;
-    var h = window.innerHeight - 50;
-    //var int = setInterval(moving,500);
-    function moving(){
-        if (counter < 5){
-          t = 0;
-          l = 0;
-          captureExample();
-          mymove(t.toString(),l.toString());
-        }
-        if( counter >= 5 && counter < 10){
-          t = 0;
-          l = w;
-          captureExample();
-          mymove(t.toString(),l.toString());
-        }
-        if( counter >= 10 && counter < 15){
-          t = h;
-          l = 0;
-          captureExample();
-          mymove(t.toString(),l.toString());
-        }
-        if( counter >= 15 && counter < 20){
-          t = h;
-          l = w;
-          captureExample();
-          mymove(t.toString(),l.toString());
-        }
-        if( counter >= 20 && counter < 25){
-            t = h/2;
-            l = w/2;
-            captureExample();
-            mymove(t.toString(),l.toString());
+// function circlemove(){
+//     var l = 1;
+//     var t = 0;
+//     var sw = false;
+//     var counter = 0;
+//     var w = window.innerWidth - 50;
+//     var h = window.innerHeight - 50;
+//     //var int = setInterval(moving,500);
+//     function moving(){
+//         if (counter < 5){
+//           t = 0;
+//           l = 0;
+//           captureExample();
+//           mymove(t.toString(),l.toString());
+//         }
+//         if( counter >= 5 && counter < 10){
+//           t = 0;
+//           l = w;
+//           captureExample();
+//           mymove(t.toString(),l.toString());
+//         }
+//         if( counter >= 10 && counter < 15){
+//           t = h;
+//           l = 0;
+//           captureExample();
+//           mymove(t.toString(),l.toString());
+//         }
+//         if( counter >= 15 && counter < 20){
+//           t = h;
+//           l = w;
+//           captureExample();
+//           mymove(t.toString(),l.toString());
+//         }
+//         if( counter >= 20 && counter < 25){
+//             t = h/2;
+//             l = w/2;
+//             captureExample();
+//             mymove(t.toString(),l.toString());
            
-        }
-        if(counter >= 25 ){
-          fitModel();
-          setInterval(moveTarget, 100);
-          clearInterval(int);
-        }
-        counter++;
-        // if(l%100 == 0 || t % 100 == 0) 
-        //     captureExample();
-        // if (!sw){
-        //     if(l == 0 && t == 0)
-        //         sw = true;
-        //     if(l < w && t == 0)
-        //         l++
-        //     if(l == w && t < h )
-        //         t++
-        //     if( l > 0 && t == h )
-        //         l--
-        //     if(l == 0 && t > 0)
-        //         t--
-        //     mymove(t.toString(),l.toString());
+//         }
+//         if(counter >= 25 ){
+//           fitModel();
+//           setInterval(moveTarget, 100);
+//           clearInterval(int);
+//         }
+//         counter++;
+//         // if(l%100 == 0 || t % 100 == 0) 
+//         //     captureExample();
+//         // if (!sw){
+//         //     if(l == 0 && t == 0)
+//         //         sw = true;
+//         //     if(l < w && t == 0)
+//         //         l++
+//         //     if(l == w && t < h )
+//         //         t++
+//         //     if( l > 0 && t == h )
+//         //         l--
+//         //     if(l == 0 && t > 0)
+//         //         t--
+//         //     mymove(t.toString(),l.toString());
 
-        // }
-        // else { 
-        //     t = h/2;
-        //     l = w/2;
-        //     captureExample();
-        //     mymove(t.toString(),l.toString());
-        //     fitModel();
-        //     setInterval(moveTarget, 100);
-        //     clearInterval(int);
-        // }
-    }
-    // function captureExample(t,l) {
-    //     // Take the latest image from the eyes canvas and add it to our dataset.
-    //     tf.tidy(function() {
-    //       const image = getImage();
-    //     //   const circPos = tf.tensor1d([l, t]).expandDims(0);
-    //       const circPos = tf.tensor1d([(l / $(window).width()) * 2 - 1, (t / $(window).width())    * 2 - 1]).expandDims(0);
+//         // }
+//         // else { 
+//         //     t = h/2;
+//         //     l = w/2;
+//         //     captureExample();
+//         //     mymove(t.toString(),l.toString());
+//         //     fitModel();
+//         //     setInterval(moveTarget, 100);
+//         //     clearInterval(int);
+//         // }
+//     }
+//     // function captureExample(t,l) {
+//     //     // Take the latest image from the eyes canvas and add it to our dataset.
+//     //     tf.tidy(function() {
+//     //       const image = getImage();
+//     //     //   const circPos = tf.tensor1d([l, t]).expandDims(0);
+//     //       const circPos = tf.tensor1d([(l / $(window).width()) * 2 - 1, (t / $(window).width())    * 2 - 1]).expandDims(0);
       
-    //       // Choose whether to add it to training (80%) or validation (20%) set:
-    //       const subset = dataset[Math.random() > 0.2 ? 'train' : 'val'];
+//     //       // Choose whether to add it to training (80%) or validation (20%) set:
+//     //       const subset = dataset[Math.random() > 0.2 ? 'train' : 'val'];
       
-    //       if (subset.x == null) {
-    //         // Create new tensors
-    //         subset.x = tf.keep(image);
-    //         subset.y = tf.keep(circPos);
-    //       } else {
-    //         // Concatenate it to existing tensors
-    //         const oldX = subset.x;
-    //         const oldY = subset.y;
+//     //       if (subset.x == null) {
+//     //         // Create new tensors
+//     //         subset.x = tf.keep(image);
+//     //         subset.y = tf.keep(circPos);
+//     //       } else {
+//     //         // Concatenate it to existing tensors
+//     //         const oldX = subset.x;
+//     //         const oldY = subset.y;
       
-    //         subset.x = tf.keep(oldX.concat(image, 0));
-    //         subset.y = tf.keep(oldY.concat(circPos, 0));
-    //       }
+//     //         subset.x = tf.keep(oldX.concat(image, 0));
+//     //         subset.y = tf.keep(oldY.concat(circPos, 0));
+//     //       }
       
-    //       // Increase counter
-    //       subset.n += 1;
-    //       if(subset.n > 25){
-    //         fitModel()
-    //       }
-    //     });
-    //   }
+//     //       // Increase counter
+//     //       subset.n += 1;
+//     //       if(subset.n > 25){
+//     //         fitModel()
+//     //       }
+//     //     });
+//     //   }
     
-  }
+//   }
--- statics/main.js
+++ statics/main.js
@@ -26,63 +26,63 @@ let data_positions = [
     //1
   },
   {
-    x: window.innerWidth/2,
-    y: window.innerHeight,
+    x: (window.innerWidth-100)/2,
+    y: (window.innerHeight-100),
     //2
   },
   {
-    x: window.innerWidth,
-    y: window.innerHeight/2,
+    x: (window.innerWidth-100),
+    y: (window.innerHeight-100)/2,
     //3
   },
   {
-    x: window.innerWidth/2,
+    x: (window.innerWidth-100)/2,
     y: 0,
     //4
   },
   {
     x: 0,
-    y: window.innerHeight,
+    y: (window.innerHeight-100),
     //5
   },
   {
     x: 0,
-    y: window.innerHeight/2,
+    y: (window.innerHeight-100)/2,
     //6
   },
   {
-    x: window.innerWidth,
-    y: window.innerHeight,
+    x: (window.innerWidth-100),
+    y: (window.innerHeight-100),
     //7
   },
   {
-    x:window.innerWidth,
+    x:(window.innerWidth-100),
     y: 0,
     //8
   },
   {
-    x: window.innerWidth/2,
-    y: window.innerWidth/2,
+    x: (window.innerWidth-100)/2,
+    y: (window.innerHeight-100)/2,
     //9
   },
   {
-    x: window.innerWidth * 0.3,
-    y: window.innerHeight * 0.7,
+    x: (window.innerWidth-100) * 0.3,
+    y: (window.innerHeight-100) * 0.7,
     //10
   },
   {
-    x: window.innerWidth * 0.7,
-    y: window.innerHeight * 0.3,
+    x: (window.innerWidth-100) * 0.7,
+    y: (window.innerHeight-100) * 0.3,
     //11
   },
   {
-    x: window.innerWidth * 0.7,
-    y: window.innerHeight * 0.7,
+    x: (window.innerWidth-100) * 0.7,
+    y: (window.innerHeight-100) * 0.7,
     //12
   },
   {
-    x: window.innerWidth * 0.3,
-    y: window.innerHeight * 0.3,
+    x: (window.innerWidth-100) * 0.3,
+    y: (window.innerHeight-100) * 0.3,
   },
 ];
 
@@ -92,10 +92,10 @@ function myMove(t,l){
   circle.css('top',t + 'px'); 
 }
 let data_iterator = 0;
-function captureExample(interval,l,t){
-  clearInterval(interval);
+function captureExample(interval){
+  //clearInterval(interval);
   
-  const circPos = tf.tensor1d([(l / $(window).width()) * 2 - 1, (t / $(window).width())    * 2 - 1]).expandDims(0);
+  const circPos = tf.tensor1d([(data_positions[data_iterator].x / $(window).width()) * 2 - 1, (data_positions[data_iterator].y / $(window).width())    * 2 - 1]).expandDims(0);
   const pupil_pos = tf.tensor1d([(pupilx / eye_size.x * 2 - 1), (pupily / eye_size.y    * 2 - 1)]).expandDims(0);
   // Choose whether to add it to training (80%) or validation (20%) set:
   const subset = dataset[Math.random() > 0.2 ? 'train' : 'val'];
@@ -117,16 +117,16 @@ function captureExample(interval,l,t){
   subset.n += 1;
 
 
-  move_interval = setInterval(moveCircle, 400, move_interval);
+  //move_interval = setInterval(moveCircle, 1000, move_interval);
 
 }
 function moveCircle(interval){
-  if(data_iterator == data_positions){
+  if(data_iterator == data_positions.length){
     clearInterval(interval);
-
+   // alert(data_positions);
   }
-  clearInterval(interval);
-  captureExample_interval = setInterval(captureExample, 300, captureExample_interval,data_positions[data_iterator].x,data_positions[data_iterator].y);
+  //clearInterval(interval);
+  //captureExample_interval = setInterval(captureExample, 1000, captureExample_interval,data_positions[data_iterator].x,data_positions[data_iterator].y);
   myMove(data_positions[data_iterator].x,data_positions[data_iterator].y);
   data_iterator++;
 }
@@ -135,70 +135,76 @@ $(document).ready(function() {
   
 
    
-    // $("#calibrate").click(function collect_data(){
+    $("#calibrate").click(function collect_data(){
 
-    //   move_interval = setInterval(moveCircle, 400, move_interval);
+      move_interval = setInterval(moveCircle, 1000, move_interval);
+      captureExample_interval = setInterval(captureExample, 1000, captureExample_interval);
     
-    
-    // }); 
+    }); 
             
   const video = $('#webcam')[0];
-
-  function getEyesRectangle(positions) {
-
-  let minX = positions[23][0] - 5;
-  let maxX = positions[28][0] + 5;
-  let minY = positions[24][1] - 5;
-  let maxY = positions[31][1] + 5;
-
   
-
-
-  const width = maxX - minX;
-  const height = maxY - minY;
-
-  return [minX, minY, width, height];
-  }
-  function onStreaming(stream) {
-    video.srcObject = stream;
-    const ctrack = new clm.tracker();
-    ctrack.init();
-    ctrack.start(video);
-    const overlay = $('#overlay')[0];
-    const overlayCC = overlay.getContext('2d');
-
-
-    
-    function trackingLoop() {
-      // Check if a face is detected, and if so, track it.
-      requestAnimationFrame(trackingLoop);
-
-      let currentPosition = ctrack.getCurrentPosition();
-      overlayCC.clearRect(0, 0, 400, 300);
-      if (currentPosition) {
-        // Draw facial mask on overlay canvas:
-        ctrack.draw(overlay);
-        // Get the eyes rectangle and draw it in red:
-        const eyesRect = getEyesRectangle(currentPosition);
-        overlayCC.strokeStyle = 'red';
-        overlayCC.strokeRect(eyesRect[0], eyesRect[1], eyesRect[2], eyesRect[3]);
-
-        // The video might internally have a different size, so we need these
-        // factors to rescale the eyes rectangle before cropping:
-        const resizeFactorX = video.videoWidth / video.width;
-        const resizeFactorY = video.videoHeight / video.height;
-
-        // Crop the eyes from the video and paste them in the eyes canvas:
-        const eyesCanvas = $('#eyes')[0];
-        const eyesCC = eyesCanvas.getContext('2d');
-
-        eyesCC.drawImage(
-          video,
-          eyesRect[0] * resizeFactorX, eyesRect[1] * resizeFactorY,
-          eyesRect[2] * resizeFactorX, eyesRect[3] * resizeFactorY,
-          0, 0, eyesCanvas.width, eyesCanvas.height
-        );
-
+ 
+  function getEyesRectangle(positions) {
+    // const minX = positions[23][0] - 5;
+    // const maxX = positions[28][0] + 5;
+    // const minY = positions[24][1] - 5;
+    // const maxY = positions[26][1] + 5;
+  
+  
+    let minX = positions[23][0] - 5;
+    let maxX = positions[28][0] + 5;
+    let minY = positions[24][1] - 5;
+    let maxY = positions[31][1] + 5;
+    // if (maxY < positions[26][1]){
+    //   maxY = positions[26][1] + 5;
+    // }
+    const width = maxX - minX;
+    const height = maxY - minY;
+  
+    return [minX, minY, width, height];
+    }
+    function onStreaming(stream) {
+      video.srcObject = stream;
+      const ctrack = new clm.tracker();
+      ctrack.init();
+      ctrack.start(video);
+      const overlay = $('#overlay')[0];
+      const overlayCC = overlay.getContext('2d');
+  
+  
+        console.log('hey');
+       
+      function trackingLoop() {
+        // Check if a face is detected, and if so, track it.
+        requestAnimationFrame(trackingLoop);
+  
+        let currentPosition = ctrack.getCurrentPosition();
+        overlayCC.clearRect(0, 0, 400, 300);
+        if (currentPosition) {
+      // Draw facial mask on overlay canvas:
+      ctrack.draw(overlay);
+  
+      // Get the eyes rectangle and draw it in red:
+      const eyesRect = getEyesRectangle(currentPosition);
+      overlayCC.strokeStyle = 'red';
+      overlayCC.strokeRect(eyesRect[0], eyesRect[1], eyesRect[2], eyesRect[3]);
+  
+      // The video might internally have a different size, so we need these
+      // factors to rescale the eyes rectangle before cropping:
+      const resizeFactorX = video.videoWidth / video.width;
+      const resizeFactorY = video.videoHeight / video.height;
+  
+      // Crop the eyes from the video and paste them in the eyes canvas:
+      const eyesCanvas = $('#eyes')[0];
+      const eyesCC = eyesCanvas.getContext('2d');
+  
+      eyesCC.drawImage(
+        video,
+        eyesRect[0] * resizeFactorX, eyesRect[1] * resizeFactorY,
+        eyesRect[2] * resizeFactorX, eyesRect[3] * resizeFactorY,
+        0, 0, eyesCanvas.width, eyesCanvas.height
+      );
         //print pupil position in p tag
         pupilx = currentPosition[23][0] - currentPosition[27][0];
         pupily = currentPosition[26][1] - currentPosition[27][1];
